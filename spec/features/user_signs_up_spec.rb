3aeff89a7ba1d0212626e77ecb237b25b133054b
Sean Fitzgerald <seanfitzgerald@Seans-MacBook.local>
2018-02-05 20:45:57 -0500

    added name to users, updated sign up form and tests to account, and made slight presentation changes to authentication pages along with updated tests for each

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 
 feature 'user registers', %Q{
-  As a visitor
-  I want to register
-  So that I can create an account
+  As a prospective user
+  I want to create an account
+  So that I can begin to track my wellness
 } do
 
   # Acceptance Criteria:
   # * I must specify a valid email address,
   #   password, and password confirmation
+  # * I must specify a first name
   # * If I don't specify the required information, I am presented with
   #   an error message
+  # * If I don't match my passwords, I am presented with an error message
 
   scenario 'provide valid registration information' do
     visit new_user_registration_path
 
+    fill_in 'First Name', with: 'John'
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
 
-    click_button 'Sign up'
+    click_button 'Sign Up'
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
     expect(page).to have_content('Sign Out')
@@ -28,8 +31,25 @@ feature 'user registers', %Q{
   scenario 'provide invalid registration information' do
     visit new_user_registration_path
 
-    click_button 'Sign up'
-    expect(page).to have_content("can't be blank")
+    click_button 'Sign Up'
+
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to_not have_content('Sign Out')
+  end
+
+  scenario 'provide mismatching passwords and confirmation' do
+    visit new_user_registration_path
+
+    fill_in 'First Name', with: 'John'
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password1'
+    fill_in 'Password Confirmation', with: 'password2'
+
+    click_button 'Sign Up'
+
+    expect(page).to have_content("Password confirmation doesn't match Password")
     expect(page).to_not have_content('Sign Out')
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:name).when('John') }
+  it { should_not have_valid(:name).when(nil, '', ' ') }
+
+  it { should have_valid(:email).when('user@example.com') }
+  it { should_not have_valid(:email).when(nil, /\s*/) }
+
+  it { should have_valid(:password).when('qwerty') }
+  it { should_not have_valid(:password).when('qwer') }
+
+  it 'has a matching password confirmation' do
+    user = User.new
+    user.password = 'password'
+    user.password_confirmation = 'anotherpassword'
+
+    expect(user).to_not be_valid
+    expect(user.errors[:password_confirmation]).to_not be_blank
+  end
 end

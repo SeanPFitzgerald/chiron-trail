2a38f57602d16c85123f6eaf19f988c6707f2fba
Sean Fitzgerald <seanfitzgerald@Seans-MacBook.local>
2018-02-12 17:53:03 -0500

    removed migration that changes providers column name as it was conflicting with other branch doing the same thing

--- db/migrate/20180212202344_change_providers_column_name.rb
+++
@@ -1,9 +0,0 @@
-class ChangeProvidersColumnName < ActiveRecord::Migration[5.1]
-  def up
-    rename_column :providers, :type, :provider_type
-  end
-
-  def down
-    rename_column :providers, :provider_type, :type
-  end
-end

8dbe19d43f34d3290d7b451ab2c83c776d2bfc03
Sean Fitzgerald <seanfitzgerald@Seans-MacBook.local>
2018-02-15 20:00:09 -0500

    fixed test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,8 +11,7 @@ RSpec.describe User, type: :model do
   it { should_not have_valid(:password).when('qwer') }
 
   it { should have_valid(:phone_number).when('+12345678901') }
-  it { should_not have_valid(:phone_number).when('12345678901', '1234567890123') }
-
+  
   it { should have_many(:prescriptions) }
   it { should have_many(:medications).through(:prescriptions) }
 

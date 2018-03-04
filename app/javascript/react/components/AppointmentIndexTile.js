5c3a6f04186f16e3349ee82c105c844fd9e01909
Sean Fitzgerald <seanfitzgerald@Seans-MacBook.local>
2018-03-04 16:58:08 -0500

    updated view of the appointments index, allowed editing of appointments from within index, updated appointments api controller to reflect these modifications

--- app/javascript/react/components/AppointmentIndexTile.js
+++ app/javascript/react/components/AppointmentIndexTile.js
@@ -3,17 +3,54 @@ import React from 'react'
 const AppointmentIndexTile = (props) => {
   return(
     <div>
-      <li>
-        <strong>Description:</strong> {props.appointmentName}
-        <span className='floatRight'>
-          <strong>Provider:</strong> {props.providerName}
-        </span>
-        <ol>
-          Start Date: {props.date}<br />
-          Time: {props.time}<br />
-          Notes: {props.notes}<br />
-        </ol>
-      </li><br />
+      <hr />
+      <div className='row'>
+        <div className='small-10 large-10 columns'>
+          <div className='row'>
+            <div className='small-3 large-3 columns'>
+              <strong className='tile-title right'>Description: </strong>
+            </div>
+            <div className='small-8 large-8 columns left'>
+              <span className='tile-info'>{props.appointmentName}</span><br />
+            </div>
+          </div>
+          <div className='row'>
+            <div className='small-3 large-3 columns'>
+              <strong className='tile-title right'>Provider: </strong>
+            </div>
+            <div className='small-8 large-8 columns left'>
+              <span className='tile-info'>{props.providerName}</span><br />
+            </div>
+          </div>
+          <div className='row'>
+            <div className='small-3 large-3 columns'>
+              <strong className='tile-title right'>Start Date: </strong>
+            </div>
+            <div className='small-8 large-8 columns left'>
+              <span className='tile-info'>{props.date.format('ddd, MMM Do YYYY')}</span><br />
+            </div>
+          </div>
+          <div className='row'>
+            <div className='small-3 large-3 columns'>
+              <strong className='tile-title right'>Time: </strong>
+            </div>
+            <div className='small-8 large-8 columns left'>
+              <span className='tile-info'>{props.time.format('h:mm a')}</span><br />
+            </div>
+          </div>
+          <div className='row'>
+            <div className='small-3 large-3 columns'>
+              <strong className='tile-title right'>Notes: </strong>
+            </div>
+            <div className='small-8 large-8 columns left'>
+              <span className='tile-info'>{props.notes}</span><br />
+            </div>
+          </div>
+        </div>
+        <div className='small-2 large-2 columns'>
+          <button className='button tiny' id={props.id} onClick={props.handleEdit}>Edit</button>
+        </div>
+      </div>
     </div>
   )
 }
